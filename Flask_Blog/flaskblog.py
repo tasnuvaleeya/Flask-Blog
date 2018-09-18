11f1031f599fac6c1647abbe5d0ca4726a28fc11
tasnuvaleeya <tasnuva2606@gmail.com>
2018-09-18 13:26:56 +0600

    teamplate and styling added

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,11 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author' : 'Tasnuva Zaman',
+        'title' : 'Blog Post 1',
+        'content' : 'first post content',
+        'date_posted' : 'September 14, 2018'
+    },
+    {
+        'author': 'Shahriar Siddik',
+        'title': 'Blog Post 2',
+        'content': 'second post content',
+        'date_posted': 'September 17, 2018'
+    }
+]
 
 @app.route('/')
-def hello():
-    return '<h1>Homepage!</h1>'
+@app.route('/home')
+def home():
+    return render_template('blog/home.html', posts=posts)
+
+
+@app.route('/about')
+def about():
+    return render_template('blog/about.html')
+
 
 
 
